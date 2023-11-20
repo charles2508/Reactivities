5484d5c362c3a58f923e42c05cc992579420792e
Charles2508 <charles2508@users.noreply.github.com>
2023-11-20 21:43:42 +1100

    Add application layer details

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,27 +1,43 @@
-using System.Diagnostics;
+using Application.Activities;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
-
         [HttpGet] //api/activities
-        public async Task<ActionResult<List<Activity>>> GetActivities() 
+        public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return Ok( await _context.Activities.ToListAsync());
+            return Ok(await Mediator.Send(new List.Query()));
         }
 
         [HttpGet("{id}")] //api/activities/1
-        public async Task<ActionResult<Activity>> GetActivity(Guid id) {
-            return Ok( await _context.Activities.FindAsync(id));
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Details.Query { Id = id }));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateActivty(Activity activity)
+        {
+            await Mediator.Send(new Create.Command { Activity = activity });
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Edit(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            await Mediator.Send(new Edit.Command { Id = id, Activity = activity });
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            await Mediator.Send(new Delete.Command { Id = id });
+            return Ok();
         }
     }
 }
